61871764135bef9bb9dec377dccc3c023d1f3198
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-07-05 10:20:55 -0400

    setup basic codebase

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* WHITESPACE = " \t\n\v\f\r";
+
+int main(int argc, char* argv[]) {
+	// open code file
+	FILE* codeFile = fopen(argv[1], "r");
+	if (!codeFile) {
+		fprintf(stderr, "No file specified");
+		return 1;
+	}
+	// get code file length
+	fseek(codeFile, 0, SEEK_END);
+	long codeFileLength = ftell(codeFile);
+	fseek(codeFile, 0, SEEK_SET);
+	// store code into string and close file
+	char* codeText = malloc(codeFileLength);
+	fread(codeText, codeFileLength, 1, codeFile);
+	fclose(codeFile);
+
+	puts(codeText);
+
+	return 0;
+}
